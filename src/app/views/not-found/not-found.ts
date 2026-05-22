f9f5472b8b95fce726e512702df8243ba340df9d
Ahnx33 <andreayang2007@gmail.com>
2026-05-22 23:07:14 +0200

    style: design 404 not-found page

--- src/app/views/not-found/not-found.ts
+++ src/app/views/not-found/not-found.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterLink, MatButtonModule, MatIconModule],
   templateUrl: './not-found.html',
   styleUrl: './not-found.css',
 })
-export class NotFound {
-
-}
+export class NotFound {}
